2097d456aa2eb91227ffbc1b021f26266b02ad3d
Moses Gitau <gitaumoses4@gmail.com>
2018-05-30 00:26:10 +0300

    [Feature #157933384] Update validation errors to use JSend Format

--- API/data_store/__init__.py
+++ API/data_store/__init__.py
@@ -79,27 +79,27 @@ class UserCollection(NonPersistentCollection):
     def is_valid(self, item):
         errors = []
         if not item.get("firstname"):
-            errors.append("First name is required")
+            errors.append({"firstname": "First name is required"})
 
         if not item.get("lastname"):
-            errors.append("Last name is required")
+            errors.append({"lastname": "Last name is required"})
 
         if not item.get("username"):
-            errors.append("Username is required")
+            errors.append({"username": "Username is required"})
         elif self.query_by_field(field="username", value=item.get("username")) is not None:
-            errors.append("Username already in use")
+            errors.append({"username": "Username already in use"})
 
         if not item.get("email"):
-            errors.append("Email is required")
+            errors.append({"email": "Email is required"})
         elif re.match(item.get("email"), '^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$') is None:
-            errors.append("Not a valid email")
+            errors.append({"email": "Not a valid email"})
         elif self.query_by_field(field="email", value=item.get("email")) is not None:
-            errors.append("Email already in use")
+            errors.append({"email": "Email already in use"})
 
         if not item.get("password"):
-            errors.append("Password is required")
+            errors.append({"password": "Password is required"})
         elif len(item.get("password")) < 8:
-            errors.append("Password must be more than 8 characters long")
+            errors.append({"password": "Password must be more than 8 characters long"})
 
         return len(errors) == 0, errors
 
@@ -108,10 +108,10 @@ class RequestCollection(NonPersistentCollection):
     def is_valid(self, item):
         errors = []
         if not item.get("product_name"):
-            errors.append("Product name must be provided")
+            errors.append({"product_name": "Product name must be provided"})
 
         if not item.get("description"):
-            errors.append("Maintenance/Repair request description must be provided")
+            errors.append({"description": "Maintenance/Repair request description must be provided"})
 
         return len(errors) == 0, errors
 
@@ -120,7 +120,7 @@ class NotificationCollection(NonPersistentCollection):
     def is_valid(self, item):
         errors = []
         if not item.get("message"):
-            errors.append("Notification message must be provided")
+            errors.append({"message": "Notification message must be provided"})
 
         return len(errors) == 0, errors
 
@@ -129,6 +129,6 @@ class FeedbackCollection(NonPersistentCollection):
     def is_valid(self, item):
         errors = []
         if not item.get("message"):
-            errors.append("Feedback message must be provided")
+            errors.append({"message": "Feedback message must be provided"})
 
         return len(errors) == 0, errors
