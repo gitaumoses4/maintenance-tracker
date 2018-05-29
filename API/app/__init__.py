ac0799166834b7361b4192596ef9d1ae15c498fd
Moses Gitau <gitaumoses4@gmail.com>
2018-05-30 01:35:55 +0300

    [Feature #157933447] Create user login functionality and integrate JWT

--- API/app/__init__.py
+++ API/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from config import config
-from routes import routes
+from routes import routes, db
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -9,5 +10,12 @@ def initialize_app(config_name="DEVELOPMENT"):
     """ Load the app configuration"""
     app.config.from_object(config[config_name])
     app.register_blueprint(routes, url_prefix="/api/v1")
+
+    # initialize JWT
+    jwt = JWTManager(app)
+
+    @jwt.token_in_blacklist_loader
+    def check_token(token):
+        return token['jti'] in db.blacklist
 
     return app
--- API/routes/__init__.py
+++ API/routes/__init__.py
@@ -14,27 +14,71 @@ routes = Blueprint("routes", __name__)
 db = MaintenanceTrackerDB()
 
 
-@routes.route("/users/signup", methods=["POST"])
-def register_user():
+def is_json():
     if not request.is_json:
         return jsonify({
             "message": "Request should be in JSON",
             "status": "error"
         }), 400
-    valid, errors = db.users.is_valid(request.json)
-    if not valid:
-        return jsonify({
-            "data": errors,
-            "status": "error"
-        }), 400
-    # create user
-    result = request.json
-    user = User(result['firstname'], result['lastname'], result['email'], result['username'], result['password'])
+    return True
 
-    db.users.insert(user)
-    return jsonify({
-        "data": {
-            "user": user.to_json_object()
-        },
-        "status": "success"
-    }), 201
+
+@routes.route("/users/signup", methods=["POST"])
+def register_user():
+    if is_json():
+        valid, errors = db.users.is_valid(request.json)
+        if not valid:
+            return jsonify({
+                "data": errors,
+                "status": "error"
+            }), 400
+        # create user
+        result = request.json
+        user = User(result['firstname'], result['lastname'], result['email'], result['username'], result['password'])
+
+        db.users.insert(user)
+        return jsonify({
+            "data": {
+                "user": user.to_json_object()
+            },
+            "status": "success"
+        }), 201
+
+
+@routes.route("/users/login", methods=["POST"])
+def login_user():
+    if is_json():
+        if not request.json.get('username'):
+            return jsonify({
+                "status": "error",
+                "data": {
+                    "username": "Username is required"
+                }
+            }), 400
+        if not request.json.get("password"):
+            return jsonify({
+                "status": "error",
+                "data": {
+                    "email": "Password is required"
+                }
+            }), 400
+
+        user = db.users.query_by_field("username", request.json.get("username"))
+        if not user:
+            return jsonify({
+                "status": "error",
+                "message": "Username does not exist"
+            }), 400
+        elif not bcrypt.verify(request.json.get("password"), user.password):
+            return jsonify({
+                "status": "error",
+                "message": "Wrong password"
+            }), 400
+
+        access_token = create_access_token(identity=user.username)
+        return jsonify({
+            "status": "success",
+            "data": {
+                "token": access_token
+            }
+        }), 200
