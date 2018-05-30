674173620f8581051b5ab21c4ea1d7c5cf26f931
Moses Gitau <gitaumoses4@gmail.com>
2018-05-30 15:18:31 +0300

    [Chore #157965588] Refactor tests for authentication

--- API/tests/test_login_and_logout.py
+++ API/tests/test_login_and_logout.py
@@ -1,4 +1,5 @@
 from tests.base_test import BaseTestCase
+import json
 
 
 class LoginTestCase(BaseTestCase):
@@ -10,37 +11,64 @@ class LoginTestCase(BaseTestCase):
             "password": "admin"
         }
         self.user = {
+            "firstname": "Moses",
+            "lastname": "Gitau",
+            "email": "gitaumoses@gmail.com",
             "username": "gitaumoses",
-            "password": "andela"
+            "password": "password"
         }
 
     def test_user_can_login(self):
         result = self.client().post(self.full_endpoint('users/signup'), data=self.user, headers=self.headers)
         self.assertEqual(result.status_code, 201)
 
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
+
         result = self.client().post(self.full_endpoint('users/login'), data=self.user, headers=self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
 
     def test_admin_can_login(self):
         result = self.client().post(self.full_endpoint('admin/login'), self.admin, self.headers)
         self.assertEqual(result.status_code, 200)
 
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
+
     def test_user_can_logout(self):
         result = self.client().post(self.full_endpoint('users/signup'), data=self.user, headers=self.headers)
         self.assertEqual(result.status_code, 201)
 
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
+
         result = self.client().post(self.full_endpoint('users/login'), data=self.user, headers=self.headers)
         self.assertEqual(result.status_code, 200)
 
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
+
         result = self.client().delete(self.full_endpoint('users/logout'), self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
 
     def test_admin_can_logout(self):
         result = self.client().post(self.full_endpoint('admin/login'), data=self.admin, headers=self.headers)
         self.assertEqual(result.status_code, 200)
 
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
+
         result = self.client().delete(self.full_endpoint('admin/logout'), headers=self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_test=True))
+        self.assertEqual(json_result['status'], "success")
 
     def tearDown(self):
         super().tearDown()
