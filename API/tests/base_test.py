c65c83dc4783b94fb0afdc33b1380c82d2e2f302
Moses Gitau <gitaumoses4@gmail.com>
2018-05-30 22:46:47 +0300

    [Chore #157977831] Create passing CRUD tests for request

--- API/tests/base_test.py
+++ API/tests/base_test.py
@@ -1,5 +1,9 @@
+import json
 from unittest import TestCase
 from dotenv import load_dotenv
+
+from models.admin import Admin
+from models.user import User
 
 load_dotenv()
 from app import initialize_app, clear
@@ -12,6 +16,7 @@ class BaseTestCase(TestCase):
         self.app = initialize_app("TESTING")
         self.client = self.app.test_client
         self.headers = {'Content-Type': 'application/json'}
+        self.admin_headers = {'Content-Type': 'application/json'}
 
     def full_endpoint(self, path=""):
         return self.api_prefix + path
@@ -25,6 +30,42 @@ class AuthenticatedTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
         """Create access token for the test cases"""
+        self.admin = Admin()
+        self.admin.username = "admin"
+        self.admin.password = "admin"
+
+        self.user = User()
+        self.user.firstname = "Moses"
+        self.user.lastname = "Gitau"
+        self.user.username = "gitaumoses"
+        self.user.email = "gitaumoses@gmail.com"
+        self.user.password = "password"
+
+        self.client().post(
+            self.full_endpoint("users/signup"),
+            data=self.user.to_json_str(),
+            headers=self.headers
+        )
+
+        result = self.client().post(
+            self.full_endpoint('users/login'),
+            data=self.user.to_json_str(),
+            headers=self.headers
+        )
+        json_result = json.loads(result.get_data(as_text=True))
+
+        self.headers['Authorization'] = 'Bearer {}'.format(json_result['data']['token'])
+
+        # Login admin
+
+        result = self.client().post(
+            self.full_endpoint('admin/login'),
+            data=self.admin.to_json_str(),
+            headers=self.admin_headers
+        )
+
+        json_result = json.loads(result.get_data(as_text=True))
+        self.admin_headers['Authorization'] = 'Bearer {}'.format(json_result['data']['token'])
 
     def tearDown(self):
         pass
--- API/tests/test_requests.py
+++ API/tests/test_requests.py
@@ -1,3 +1,6 @@
+import json
+
+from models.request import Request
 from tests.base_test import AuthenticatedTestCase
 
 
@@ -5,42 +8,73 @@ class RequestsTestCase(AuthenticatedTestCase):
 
     def setUp(self):
         super().setUp()
-        self.request = {
-            "product_name": "Samsung LED TV",
-            "description": "The screen has a crack",
-            "status": "pending",
-            "photo": "",
-            "user_id": 1
-        }
+        self.request = Request(product_name="Samsung LED",
+                               description="Has a broken screen")
 
     def test_user_can_create_request(self):
-        result = self.client().post(self.full_endpoint("users/requests"), data=self.request, headers=self.headers)
+        result = self.client().post(self.full_endpoint("users/requests"),
+                                    data=self.request.to_json_str(),
+                                    headers=self.headers)
         self.assertEqual(result.status_code, 201)
+
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
 
     def test_can_get_all_requests(self):
-        result = self.client().get(self.full_endpoint("users/requests"), headers=self.headers)
+        result = self.client().get(self.full_endpoint("users/requests"),
+                                   headers=self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
 
     def test_user_can_modify_request(self):
-        result = self.client().post(self.full_endpoint("users/requests"), data=self.request, headers=self.headers)
+        result = self.client().post(self.full_endpoint("users/requests"),
+                                    data=self.request.to_json_str(),
+                                    headers=self.headers)
         self.assertEqual(result.status_code, 201)
 
-        result = self.client().patch(self.full_endpoint("users/requests/1"), data=self.request, headers=self.headers)
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
+
+        result = self.client().put(self.full_endpoint("users/requests/1"),
+                                   data=self.request.to_json_str(),
+                                   headers=self.headers)
         self.assertEqual(result.status_code, 200)
 
-        result = self.client().get(self.full_endpoint("users/requests/1"), headers=self.headers)
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
+
+        result = self.client().get(self.full_endpoint("users/requests/1"),
+                                   headers=self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
 
     def test_admin_can_modify_request(self):
-        result = self.client().patch(self.full_endpoint("admin/requests/1"), headers=self.headers)
+        result = self.client().patch(self.full_endpoint("admin/requests/1"),
+                                     headers=self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
 
     def test_can_get_request_by_id(self):
-        result = self.client().post(self.full_endpoint("users/requests"), data=self.request, headers=self.headers)
+        result = self.client().post(self.full_endpoint("users/requests"),
+                                    data=self.request.to_json_str(),
+                                    headers=self.headers)
         self.assertEqual(result.status_code, 201)
 
-        result = self.client().get(self.full_endpoint("users/requests/1"), headers=self.headers)
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
+
+        result = self.client().get(self.full_endpoint("users/requests/1"),
+                                   headers=self.headers)
         self.assertEqual(result.status_code, 200)
+
+        json_result = json.loads(result.get_data(as_text=True))
+        self.assertEqual(json_result['status'], "success")
 
     def tearDown(self):
         super().tearDown()
