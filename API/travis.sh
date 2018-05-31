d6b283860b9f641a6670fa9692355e4f8cca6615
Moses Gitau <gitaumoses4@gmail.com>
2018-05-31 03:24:42 +0300

    [Chore #157987599] Remove spaces in travis.sh environment variables

--- API/travis.sh
+++ API/travis.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # default admin first name
-export DEFAULT_ADMIN_FIRST_NAME = "Moses"
+export DEFAULT_ADMIN_FIRST_NAME="Moses"
 
 # default admin lastname
-export DEFAULT_ADMIN_LAST_NAME = "Gitau"
+export DEFAULT_ADMIN_LAST_NAME="Gitau"
 
 # default admin login username
-export DEFAULT_ADMIN_USER_NAME = "admin"
+export DEFAULT_ADMIN_USER_NAME="admin"
 
 # default admin password
-export DEFAULT_ADMIN_PASSWORD = "admin"
+export DEFAULT_ADMIN_PASSWORD="admin"
 
 # default admin email address
-export DEFAULT_ADMIN_EMAIL = "gitaumoses4@gmail.com"
+export DEFAULT_ADMIN_EMAIL="gitaumoses4@gmail.com"
 
 # path for profile picture
-export DEFAULT_ADMIN_PROFILE_PICTURE = ""
+export DEFAULT_ADMIN_PROFILE_PICTURE=""
 
 # JWT secret key
-export JWT_SECRET_KEY = "my_awesome_key"
+export JWT_SECRET_KEY="my_awesome_key"
